87d85192c4569531a104ca790aebafcc6ec1b58d
Andrey Arkhipov <arhipov11@yandex.ru>
2022-08-28 12:56:39 +0300

    added sorting

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,29 @@ import {STORAGE_KEY, FORM_ADD_TODO_INPUT} from "./utils/constants";
 import Modal from "./components/Modal.vue";
 import FormAddTodo from "./components/FormAddTodo.vue";
 const todos = ref(JSON.parse(localStorage.getItem(STORAGE_KEY)) || []);
+const selectValue = ref("date");
 watchEffect(() => {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(todos.value));
+});
+watchEffect(() => {
+  if (selectValue.value === "date") {
+    todos.value.sort((a, b) => a.id - b.id);
+  }
+  if (selectValue.value === "status") {
+    todos.value.sort((a) => {
+      if (a.done) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  }
 });
 const modalActive = ref(false);
 function toggleCompleted(todo) {
   todo.done = !todo.done;
 }
+
 function closeModal() {
   modalActive.value = false;
 }
@@ -38,12 +54,13 @@ provide("addTodo", addTodo);
       <h1 class="todoTitle">To do list</h1>
       <button class="addTodo" @click="modalActive = true"></button>
       <div><input type="text" /></div>
-      <div>
-        <select class="select">
-          <option value="1">Дата</option>
-          <option value="2">Статус</option>
-        </select>
-      </div>
+      <label class="label">
+        Сортировать по:
+        <select class="select" v-model="selectValue">
+          <option value="date">Дата</option>
+          <option value="status">Статус</option>
+        </select></label
+      >
     </header>
     <main>
       <TodosList :todos="todos" />
@@ -55,8 +72,24 @@ provide("addTodo", addTodo);
 </template>
 
 <style scoped>
+.label {
+  justify-self: end;
+  font: 400 normal 14px/132% "VS", Arial, sans-serif;
+}
 .select {
-  width: 50px;
+  width: 80px;
+  border: none;
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  background-color: #fff;
+  cursor: pointer;
+  background: url(./images/selectBg.svg) right no-repeat;
+  font: inherit;
+  padding: 0 15px 0 12px;
+}
+.select:focus {
+  outline: none;
 }
 .todoApp {
   max-width: 1300px;
@@ -64,10 +97,11 @@ provide("addTodo", addTodo);
   margin: 0 auto;
 }
 .header {
-  padding: 104px 0 30px 40px;
+  padding: 108px 0 26px 40px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(2, auto);
+  row-gap: 30px;
   border: 1px solid rgb(51, 192, 33);
 }
 .addTodo {
